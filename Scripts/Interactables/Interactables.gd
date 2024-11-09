89e2f57de0898ea03b51b74e07bfc72f7cd52e15
Riley Collard <rcol0019@student.monash.edu>
2024-11-10 06:37:20 +1100

    better interactable stuff
    better interactable stuff
    calls the node instead of the first child

--- Scripts/Interactables/Interactables.gd
+++ Scripts/Interactables/Interactables.gd
@@ -12,8 +12,8 @@ func interact(_body: Node2D) -> void:
 	
 func highlighted() -> void:
 	get_node("MainTexture").texture = highlightedTexture
-	get_node("../CanvasLayer/playergui").get_child(0).clear()
-	get_node("../CanvasLayer/playergui").get_child(0).add_text(itemDesc)
+	get_node("../CanvasLayer/playergui/itemDesc").clear()
+	get_node("../CanvasLayer/playergui/itemDesc").add_text(itemDesc)
 	
 func unhighlighted() -> void:
 	get_node("MainTexture").texture = baseTexture
