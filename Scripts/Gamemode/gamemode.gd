cd0a25e51ccfd4a4ee6d4ed048c448f372eff328
Riley Collard <rcol0019@student.monash.edu>
2025-01-21 13:32:44 +1100

    Minotaur UI fixes and level gen
    Updates to level generation for later areas (forest area)
    Works with more than just one area
    Doorways updated to update this upon leaving a boss area

--- Scripts/Gamemode/gamemode.gd
+++ Scripts/Gamemode/gamemode.gd
@@ -1,8 +1,18 @@
 extends Node
 
 var dungeon_levels_generator = []
-var dungeon_levels = ["res://Scenes/Levels/level_1.tscn", "res://Scenes/Levels/level_2.tscn", "res://Scenes/Levels/level_3.tscn"]
-var boss_level = "res://Scenes/corrupt_dragon_arena.tscn"
+var dungeon_levels = ["res://Scenes/Levels/level_1.tscn", "res://Scenes/Levels/level_2.tscn", "res://Scenes/Levels/level_3.tscn","res://Scenes/Levels/level_1.tscn", "res://Scenes/Levels/level_2.tscn", "res://Scenes/Levels/level_3.tscn", "res://Scenes/Levels/level_1.tscn", "res://Scenes/Levels/level_2.tscn", "res://Scenes/Levels/level_3.tscn", "res://Scenes/Levels/level_1.tscn", "res://Scenes/Levels/level_2.tscn", "res://Scenes/Levels/level_3.tscn"]
+
+var dungeon_area = {"level_generator" : dungeon_levels,
+					"mini_boss" : "res://Scenes/corrupt_dragon_arena.tscn",
+					"final_boss" : "res://Scenes/huntsman_boss_arena.tscn"}
+var forest_area = {"level_generator" : dungeon_levels,					#Dictionary for the areas
+					"mini_boss" : "res://Scenes/corrupt_dragon_arena.tscn",
+					"final_boss" : "res://Scenes/huntsman_boss_arena.tscn"}
+
+var area_dict = {"dungeon" : dungeon_area, #Dictionary of the dictionaries
+				"forest" : forest_area}
+var area_key = "dungeon"
 var currentLevel : int = 0
 
 var saveSlot : int
@@ -26,19 +36,16 @@ var activePodiums = {
 
 func _ready() -> void:
 	add_to_group("Persistent")
-	dungeon_levels_generator = dungeon_levels.duplicate()
-	dungeon_levels_generator.shuffle()
 	
 
 func load_level() -> void:
 	var projectiles = get_tree().get_nodes_in_group("Projectile")
 	for proj in projectiles:
 		proj.queue_free()
-	if dungeon_levels_generator.size() > currentLevel:
-		get_tree().change_scene_to_file(dungeon_levels_generator[currentLevel])
-		currentLevel += 1
-	else:
-		get_tree().change_scene_to_file(boss_level)
+	
+	get_tree().change_scene_to_file(area_dict[area_key]["level_generator"][currentLevel])
+	currentLevel += 1
+	
 	var player = PlayerData.playerScene.instantiate()
 	get_tree().root.add_child(player)
 	PlayerData.loadPlayerInfo(player)
@@ -57,3 +64,13 @@ func respawn() -> void:
 	
 func populateLootTable(key : String) -> void:
 	runtimeLootTable = lootTableDict[key].duplicate()
+
+func levelGenerator() -> void: #Level generation function, takes the level dictionary for the area
+	area_dict[area_key]["level_generator"].shuffle() #Duplicates said dungeon and shuffles it randomly
+	while area_dict[area_key]["level_generator"].size() > 2: #Out of the 15 levels it keeps the first 8
+		area_dict[area_key]["level_generator"].pop_back()
+	var midPoint = (area_dict[area_key]["level_generator"].size()/2) #Value for the mini boss index, after the halfway point minimum
+	var miniBossIndex = randi_range(midPoint, area_dict[area_key]["level_generator"].size()) #Sets its mini boss level
+	area_dict[area_key]["level_generator"].insert(miniBossIndex, area_dict[area_key]["mini_boss"])
+	area_dict[area_key]["level_generator"].append(area_dict[area_key]["final_boss"]) #Adds the final boss to the end of the index
+	print(area_dict[area_key]["level_generator"])
--- Scripts/Interactables/doorways.gd
+++ Scripts/Interactables/doorways.gd
@@ -4,6 +4,8 @@ extends "res://Scripts/Interactables/Interactables.gd"
 @onready var animator = get_node("animPlayer")
 @export var bIsUnlocked : bool = false
 @export var bHubDoor : bool = false
+@export var bBossDoor : bool = false
+@export var nextAreaKey : String
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -17,8 +19,13 @@ func _ready() -> void:
 func interact(body: Node2D)-> void:
 	if bIsUnlocked:
 		super(body)
-		if bHubDoor:
+		if bHubDoor:  #Populates table and generates the level for the first dungeon
 			Gamemode.populateLootTable(body.sceneKey)
+			Gamemode.levelGenerator()
+		elif bBossDoor: #Generates next area, changes key for level dict to new area and resets currentLevel counter
+			Gamemode.area_key = nextAreaKey
+			Gamemode.currentLevel = 0
+			Gamemode.levelGenerator()
 		PlayerData.savePlayerInfo(body.sceneKey, body)
 		Gamemode.load_level()
 		body.queue_free()
